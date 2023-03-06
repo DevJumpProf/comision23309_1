7d08f5156cfe2e5fda8ed76c4057c59d7b749277
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-03-06 20:47:46 -0300

    JS

---
+++ Codigo/Clase2/codigo.js
@@ -0,0 +1,78 @@
+// interacciones
+
+// alert
+
+/* alert ("Hola esta es mi pagina");
+
+// prompt
+
+let nombre = prompt("Decime tu nombre");
+
+
+// confirm
+
+confirm ( nombre + " queres ingresar?")
+
+
+ */
+//variables
+
+// let var const
+
+// tipos de datos
+
+/* let nombre2 = "Gabriel"  //cadena de texto = STRING
+
+nombre2= 32  // Numero = Number
+
+nombre2= false // booleano = boolean (true-false)
+
+nombre2= ["superman", "Batman"] // Array 
+
+let nombreApellido = "Juan" */
+/* document.write(nombre2) */
+
+/* console.log(nombre2)
+
+
+let numero1= 20
+
+let numero2= "20" */
+
+/* let resultado = numero1*numero2 */
+
+/* document.write(resultado) */
+
+/* let nombre = prompt("Decime tu nombre")
+let num1 = Number(prompt("ingresa un numero para sumar"))
+let num2 = Number(prompt("ingresa otro numero para sumar"))
+let resultado = num1 + num2 */
+/* 
+alert ( nombre + " el resultado de tu suma es " + resultado) */
+
+
+// const
+/* const heroe = "batman";
+heroe = "Superman"
+console.log ( heroe) */
+
+
+// operadores   +, - , / , * , %
+
+
+let num1 = 1235454
+let num2= 2
+
+/* document.write(num1 % num2)  */// resto
+
+
+// Math
+
+/* document.write(Math.pow(3,3)) */
+
+/* document.write(Math.round(6.45) ) */
+
+/* document.write(Math.floor(6.999)  ) */
+
+/* document.write(Math.ceil(6.0001)) */
+
