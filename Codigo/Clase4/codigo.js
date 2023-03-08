1b2fa5b63711f0ed9bedb264fbf7d94d5809e428
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-03-08 20:34:55 -0300

    JS ejercicios

---
+++ Codigo/Clase4/codigo.js
@@ -0,0 +1,77 @@
+/* let nombre = "Marcos";
+ console.log(nombre .length); */
+/*  console.log(nombre);
+ console.log("Hola " + nombre + " tu nombre tiene " + nombre.length  + " letras");
+
+ console.log("Hola")
+ */
+
+// operadores Comparacion - logico relacional
+
+/* let edad = 37;
+let numero = 38;
+let numero2 = "37"
+
+console.log((edad==numero2)&&(edad>numero));
+
+console.log((edad==numero2)||(edad>numero)); */
+/*        true    ||      false 
+                     true */
+
+/* let edad = 18
+
+if (edad>=18){
+console.log("sos mayor")
+}else {
+console.log("sos menor")
+}
+ */
+
+/* let nombre = prompt ("Decime tu nombre")
+
+if (edad>=18){
+    alert("sos mayor")
+    }else {
+    alert("sos menor")
+    }
+  */
+//let pelicula = "El Gran Pez"
+// if ternario
+
+//pelicula == "El Gran Pez"?console.log("Es mi peli favorita"):console.log( "NO es mi peli favorita")
+
+/* 
+if (pelicula=="El Gran Pez"){
+    console.log ("Es mi peli favorita")
+}else{
+    console.log ("NO es mi peli favorita")
+}
+ */
+
+// funciones
+/* function saludar (){
+console.log("Hola")
+}
+saludar()
+
+function sumar (num1,num2){
+    console.log(num1+num2)
+}
+sumar(10,20) */
+//
+/* let semaforo = prompt("Decime un color para el semaforo");
+
+switch (semaforo.toUpperCase()) {
+  case "ROJO":
+    alert("no podes pasar");
+    break;
+  case "AMARILLO":
+    alert("Precaucion");
+    break;
+  case "VERDE":
+    alert("podes pasar");
+    break;
+    default:
+        alert("error");
+        break;
+} */
