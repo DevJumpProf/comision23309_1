d6104e96be3a203b4b01146c9741f14ff27cfc7b
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-04-19 20:57:54 -0300

    movies

---
+++ Codigo/Clase16/movies-cac/src/App.jsx
@@ -0,0 +1,16 @@
+import { PeliculasGrid } from "./Components/PeliculasGrid";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="title">PELICULAS</header>
+      <main>
+        <PeliculasGrid/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
