f95b455eeedab05add74493b2b91bac65e804a11
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-05-31 23:38:16 -0300

    finalizado

--- Codigo/Clase25/movies-cac/src/Components/Buscador.jsx
+++ Codigo/Clase25/movies-cac/src/Components/Buscador.jsx
@@ -1,26 +1,12 @@
 import { FaSearch } from "react-icons/fa";
 import {useNavigate} from "react-router-dom" //7 importamos UseNavigate
-import {useLocation} from  "react-router-dom" //10 importamos useLocation https://reactrouter.com/en/main/hooks/use-location
+
 import "./Buscador.css"
 import { useState } from "react"; // 1 importar useState
 
 export const Buscador = () => {
 
-  //13 utilizamos este hook para tomar lo que viene por params
-  const useQuery = ()=>{
-    return new URLSearchParams(useLocation().search) //https://developer.mozilla.org/es/docs/Web/API/URLSearchParams
-  }
-
-//12 utilizo useQuery() para obtener lo que buscamos
-const query = useQuery()
-const search = query.get("search")
-/* console.log(search); */
-
-  //11- vemos en el navegador lo que viaja en la url(queryparams), separamos lo que buscamos (location.search)
-/* const location = useLocation()
-console.log(location);
-console.log(location.search); */
-
+  
   const navigate = useNavigate() // 8 defino la variable
   const [txtBuscador,setTxtBuscador] = useState ("") //2 controlamos el estado del input
 
--- Codigo/Clase25/movies-cac/src/Components/PeliculasGrid.jsx
+++ Codigo/Clase25/movies-cac/src/Components/PeliculasGrid.jsx
@@ -3,20 +3,30 @@ import {useState,useEffect} from "react"
 import { PeliculasCard } from "./PeliculasCard"
 import "./PeliculasGrid.css"
 import { Spinner } from "./Spinner.jsx"
+import {useQuery} from "../hooks/useQuery.jsx"
 
 export const PeliculasGrid=()=>{
     
 const [peliculas,setPeliculas] = useState([])
 const [cargando,setCargando]= useState(true);
 
+  //13 utilizo useQuery() para obtener lo que buscamos
+  const query = useQuery()
+  const search = query.get("search")
+  /*  console.log(search);  */
+
+
 useEffect (()=>{
     setCargando(true)
-        get("/discover/movie").then((data)=>{
+//15 hago un ternario, si hay busqueda asigna ese valor a una url sino muestra las peliculas de /discover/movie"
+       const searchURL = search?"/search/movie?query=" + search : "/discover/movie"
+
+get(searchURL).then((data)=>{
     /*         console.log(data); */
             setPeliculas(data.results);
         })
         setCargando(false)
-},[])
+},[search]) // 14 agregar search
 
   if(cargando){
     return <Spinner/>
